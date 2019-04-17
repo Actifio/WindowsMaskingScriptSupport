f12947df654f00bbe53b6ad79e805930b3bcc3c7
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-04-18 09:34:47 +1000

    Update masking_update.bat

--- masking_update.bat
+++ masking_update.bat
@@ -1,6 +1,4 @@
-REM   For direct mount workflow use     "%ACT_JOBTYPE%" == "mount"
-
-if "%ACT_JOBTYPE%" == "scrub-mount" (
+if "%ACT_MULTI_OPNAME%" == "mount" (
 
 "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -b -i "C:\Program Files\Actifio\scripts\masking_test.sql"
 IF %ERRORLEVEL% EQU 0 GOTO cleanexit
