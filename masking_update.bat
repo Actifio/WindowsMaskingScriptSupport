a0fc8fb704e3a3843f3f9687e667ebce77d543c7
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-04-17 13:43:17 +1000

    Create masking_update.bat

---
+++ masking_update.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -b -i "C:\Program Files\Actifio\scripts\masking_test.sql"
+IF %ERRORLEVEL% EQU 0 GOTO cleanexit
+exit /B 1
+
+:cleanexit
+exit /B 0
