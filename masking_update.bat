08d6cceae9a7ff58f4c2e031af337e87f14cd895
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-04-18 09:36:14 +1000

    Update masking_update.bat

--- masking_update.bat
+++ masking_update.bat
@@ -1,3 +1,5 @@
+REM  The path to SQLCMD varies according to version, it might not be 110
+
 if "%ACT_MULTI_OPNAME%" == "mount" (
 
 "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -b -i "C:\Program Files\Actifio\scripts\masking_test.sql"
