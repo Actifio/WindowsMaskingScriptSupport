b40f31a771461eb613d29f80fb8dea1ba2f7ecee
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-04-29 11:51:57 +1000

    Update masking_update.bat

--- masking_update.bat
+++ masking_update.bat
@@ -1,11 +1,15 @@
+echo off
 REM  The path to SQLCMD varies according to version, it might not be 110
 
-if "%ACT_MULTI_OPNAME%" == "scrub-mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" (
+if "%ACT_MULTI_OPNAME%" == "scrub-mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" ( GOTO maskcommand )
+if "%1" == "test" ( GOTO maskcommand )
+exit /B 0
 
-"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -b -i "C:\Program Files\Actifio\scripts\masking_test.sql"
-IF %ERRORLEVEL% EQU 0 GOTO cleanexit
+:maskcommand
+"S:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd.exe" -b -i "C:\Program Files\Actifio\scripts\simplesqlmask.sql"
+IF %ERRORLEVEL% EQU 0 (GOTO cleanexit)
 exit /B 1
-)
 
 :cleanexit
 exit /B 0
+
