484ac98c4baae9318bd1756094c0f41c783f60b3
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2019-04-29 13:54:51 +1000

    Update masking_test.sql

--- masking_test.sql
+++ masking_test.sql
@@ -1,2 +1,4 @@
-use unmaskedSmallDB
-update unmaskedSmallDB.dbo.customer set surname='smith' where id=1; 
+use premasking
+update premasking.dbo.MEMBER set LNAME='Smith' where member_id=104;
+update premasking.dbo.MEMBER set FNAME='Agent' where member_id=104; 
+update premasking.dbo.MEMBER set DISPLAY_NAME='Agent Smith';
