7123ed1aa2080c2450a8a9584a5f88fbae5e3198
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-04-21 09:10:56 +1000

    Update masking_update.bat

--- masking_update.bat
+++ masking_update.bat
@@ -3,6 +3,7 @@ REM  The path to SQLCMD varies according to version, it might not be 110
 REM  Learn the right location with:    where sqlcmd.exe
 
 if "%ACT_MULTI_OPNAME%" == "scrub-mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" ( GOTO maskcommand )
+if "%ACT_MULTI_OPNAME%" == "mount" if "%ACT_MULTI_END%" == "true" if "%ACT_PHASE%" == "post" ( GOTO maskcommand )
 if "%1" == "test" ( GOTO maskcommand )
 if "%1" == "" echo To manually run this command add test after the bat file name like this:    %0 test
 exit /B 0
