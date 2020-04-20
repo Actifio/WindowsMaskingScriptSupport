652ef8bcbf8fe6187e9921ff4fc5b0dfb3ebb199
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-04-21 09:12:09 +1000

    Delete dynamicmasking.sql

--- dynamicmasking.sql
+++
@@ -1,5 +0,0 @@
-use masksmalldb
-ALTER TABLE dbo.Customer  
-ALTER COLUMN LastName ADD MASKED WITH (FUNCTION = 'partial(2,"XXX",0)');
-CREATE USER [devlogin] FOR LOGIN [devlogin]
-GRANT SELECT ON dbo.customer TO devlogin;
